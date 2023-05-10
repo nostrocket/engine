d92f2da988f5f2478a97fc21f17c0d625427880b
gsovereignty <gsovereignty@proton.me>
2023-05-10 16:40:58 +0800

    Problem: anchor event for problem tree is not defined

--- engine/actors/config.go
+++ engine/actors/config.go
@@ -7,15 +7,23 @@ import (
 	"nostrocket/engine/library"
 )
 
+//Ignition
 const IgnitionEvent string = "fd459ea06157e30cfb87f7062ee3014bc143ecda072dd92ee6ea4315a6d2df1c"
+const IgnitionAccount library.Account = "b4f36e2a63792324a92f3b7d973fcc33eaa7720aaeee71729ac74d7ba7677675"
+
+//Anchors for incoming event trees
 const StateChangeRequests string = "7a22f580d253c4142aa4e6b28d577b2d59fdd30083b0eb27ee76a9bd750bff26"
-const ReplayPrevention string = "24c30ad7f036ed49379b5d1209836d1ff6795adb34da2d3e4cabc47dc9dfef21"
-const ConsensusTree string = "e54a960017e7ed4d485e7de34312b0f583c0a1920a2ae60d054a0ff78894fd2f"
 const Identity string = "0a73208becd0b1a9d294e6caef14352047ab44b848930e6979937fe09effaf71"
 const Shares string = "7fd9810bdb8bc635633cc4e3d0888e395420aedc7d28778c100793d1d3bc09a6"
 const Mirvs string = "c7f87218e62f6d41fa2f5b2480210ed1d48b2609e03e9b4b500a3b64e3c08554"
+const Problems string = "8d3bb1ebbaeb005857c302c81f2ffa98b952e1d59a61a0bfc11c211f38bce534"
+
+//Anchors for outgoing event trees
 const CurrentStates string = "0255594820a3ddc5b603d4e37ba6b2325879aebec401b86f9d69f5fd3864c203"
-const IgnitionAccount library.Account = "b4f36e2a63792324a92f3b7d973fcc33eaa7720aaeee71729ac74d7ba7677675"
+
+//Anchors for bidirectional event trees
+const ReplayPrevention string = "24c30ad7f036ed49379b5d1209836d1ff6795adb34da2d3e4cabc47dc9dfef21"
+const ConsensusTree string = "e54a960017e7ed4d485e7de34312b0f583c0a1920a2ae60d054a0ff78894fd2f"
 
 // InitConfig sets up our Viper config object
 func InitConfig(config *viper.Viper) {
