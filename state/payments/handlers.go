767ef9aed68118abd906b5f0ca3b5c60cb27a7b4
gsovereignty <gsovereignty@proton.me>
2023-08-02 16:27:53 +0800

    Problem: can't modify products

--- state/payments/handlers.go
+++ state/payments/handlers.go
@@ -63,31 +63,30 @@ func handleProduct(event nostr.Event) (m Mapped, err error) {
 	if ok {
 		return createProduct(event)
 	}
+	_, ok = library.GetOpData(event, "modify")
+	if ok {
+		return modifyProduct(event)
+	}
 	return Mapped{}, fmt.Errorf("event %s is attempting to change the state of products but did not contain a valid opcode", event.ID)
 }
 
 // create new product
 func createProduct(event nostr.Event) (m Mapped, err error) {
-	amount, ok := library.GetOpData(event, "amount")
-	if !ok {
-		return m, fmt.Errorf("event %s wants to create a new product but does not contain an amount", event.ID)
+	amount, err := validateAndReturnOpcodeData(event, "amount")
+	if err != nil {
+		return m, fmt.Errorf("%s tried to create a new product but %s", event.ID, err.Error())
 	}
-	sats, e := strconv.ParseInt(amount, 10, 64)
-	if e != nil {
-		return m, fmt.Errorf("event %s wants to create a new product but converting string to int failed with error %s", event.ID, e.Error())
-	}
+	sats := amount[0].(int64)
 	infoID, ok := library.GetOpData(event, "info")
 	if !ok {
 		return m, fmt.Errorf("event %s wants to create a new product but does not contain an event ID for product information", event.ID)
 	}
-	rocketID, ok := library.GetOpData(event, "rocket")
-	if !ok {
-		return m, fmt.Errorf("event %s wants to create a new product but does not contain a reocket ID", event.ID)
+	rocketQuery, err := validateAndReturnOpcodeData(event, "rocket")
+	if err != nil {
+		return m, fmt.Errorf("%s attempted to create a new product but %s", event.ID, err.Error())
 	}
-	existingRocketProducts, exists := products[rocketID]
-	if !exists {
-		existingRocketProducts = make(map[library.Sha256]Product)
-	}
+	rocketID := rocketQuery[0].(string)
+	existingRocketProducts := rocketQuery[1].(map[library.Sha256]Product)
 	existingRocketProducts[event.ID] = Product{
 		UID:                event.ID,
 		RocketID:           rocketID,
@@ -99,5 +98,81 @@ func createProduct(event nostr.Event) (m Mapped, err error) {
 }
 
 //modify existing product
+func modifyProduct(event nostr.Event) (m Mapped, err error) {
+	rocketQuery, err := validateAndReturnOpcodeData(event, "rocket")
+	if err != nil {
+		return m, fmt.Errorf("%s attempted to create a new product but %s", event.ID, err.Error())
+	}
+	rocketID := rocketQuery[0].(string)
+	existingRocketProducts := rocketQuery[1].(map[library.Sha256]Product)
+	target, err := validateAndReturnOpcodeData(event, "target")
+	if err != nil {
+		return m, fmt.Errorf("%s tried to modify a product but %s", event.ID, err.Error())
+	}
+	existingProduct, exists := existingRocketProducts[target[0].(library.Sha256)]
+	if !exists {
+		return m, fmt.Errorf("%s wants to modify a product, but this product does not exist", event.ID)
+	}
+	var updates = 0
+	if amount, err := validateAndReturnOpcodeData(event, "amount"); err == nil {
+		sats := amount[0].(int64)
+		if sats > 0 {
+			if existingProduct.Amount != sats {
+				existingProduct.Amount = sats
+				updates++
+			}
+		}
+	}
+	if infoID, ok := library.GetOpData(event, "info"); ok {
+		if len(infoID) == 64 {
+			if existingProduct.ProductInformation != infoID {
+				existingProduct.ProductInformation = infoID
+				updates++
+			}
+		}
+	}
+	if updates > 0 {
+		existingRocketProducts[target[0].(library.Sha256)] = existingProduct
+		products[rocketID] = existingRocketProducts
+		return getMapped(), nil
+	}
+	return m, fmt.Errorf("%s tried to modify a product but did not contain a valid state change", event.ID)
+}
 
-//notify payment made
+func validateAndReturnOpcodeData(event nostr.Event, opcode string) (r []any, e error) {
+	switch opcode {
+	case "target":
+		target, ok := library.GetOpData(event, "target")
+		if !ok {
+			return nil, fmt.Errorf("does not contain a target")
+		}
+		r = append(r, target)
+		return
+	case "rocket":
+		rocketID, ok := library.GetOpData(event, "rocket")
+		if !ok {
+			return nil, fmt.Errorf("does not contain a rocket ID")
+		}
+		r = append(r, rocketID)
+		existing, exists := products[rocketID]
+		if !exists {
+			existing = make(map[library.Sha256]Product)
+		}
+		r = append(r, existing)
+		return
+	case "amount":
+		amount, ok := library.GetOpData(event, "amount")
+		if !ok {
+			return r, fmt.Errorf("does not contain an amount")
+		}
+		sats, e := strconv.ParseInt(amount, 10, 64)
+		if e != nil {
+			return r, fmt.Errorf("converting amount in string to int failed with error %s", e.Error())
+		}
+		r = append(r, sats)
+		return
+	}
+	return
+}
+
+//todo notify payment made
